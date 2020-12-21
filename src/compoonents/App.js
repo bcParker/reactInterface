89747ada155840515650f62591a415fab8262ab3
Chase <cparker0820@gmail.com>
2020-12-21 16:51:40 -0500

    React state experimenting

--- src/compoonents/App.js
+++ src/compoonents/App.js
@@ -2,23 +2,36 @@ import React, {Component} from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments/>
-              <div>Search Appointments</div>
-              <div>List Appointments</div>
+class App extends Component {
+
+  constructor() {
+    super(); //this method allows us to get information from parent component by using "this" keyword
+    this.state = {
+      myName: "Chase Parker"
+    }
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <AddAppointments/>
+                <SearchAppointments/>
+                <ListAppointments/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
