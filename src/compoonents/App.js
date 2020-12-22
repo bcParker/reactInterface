db0a24ea361d813a0661ca11e042d0cbe6e9157a
Chase <cparker0820@gmail.com>
2020-12-21 21:41:46 -0500

    added first props from App.js to ListAppointments.js

--- src/compoonents/App.js
+++ src/compoonents/App.js
@@ -10,21 +10,38 @@ class App extends Component {
   constructor() {
     super(); //this method allows us to get information from parent component by using "this" keyword
     this.state = {
-      myName: "Chase Parker"
-    }
+      myName: "Chase Parker",
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const appts = result.map(item => {
+        return item;
+      })
+      this.setState({
+        myAppointments: appts
+      });
+    });
+
   }
 
   render() {
+
+
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                {this.state.myName}
                 <AddAppointments/>
                 <SearchAppointments/>
-                <ListAppointments/>
+                <ListAppointments appointments={this.state.myAppointments}/>
               </div>
             </div>
           </div>
